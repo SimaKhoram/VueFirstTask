e304ce2fb4917d8d84a320db3fb3dc4f90ade24d
Sima Khoram <simakhoram7@gmail.com>
2023-07-08 12:21:44 +0330

    did the second task - #https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: '“A pair of warm, fuzzy socks”'
+    description: '“A pair of warm, fuzzy socks”',
+    imageSrc: './assets/vmSocks-green.png',
+    imageAlt: 'A pair of socks',
+    linkHref: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding'
   }
 })
