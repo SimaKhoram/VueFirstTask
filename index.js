01f41c76e055d97372a7e23e33e9dd8367ecc017
Sima Khoram <simakhoram7@gmail.com>
2023-07-08 12:45:04 +0330

    did the third task - #https://www.vuemastery.com/courses/intro-to-vue-js/conditional-rendering

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var app = new Vue({
     description: '“A pair of warm, fuzzy socks”',
     imageSrc: './assets/vmSocks-green.png',
     imageAlt: 'A pair of socks',
-    linkHref: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding'
+    linkHref: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
+    inStock: true,
+    onSale: true
   }
 })
