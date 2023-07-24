97da82fc98b5ab74ccd831bb1bc3170788b5e14f
Sima Khoram <simakhoram7@gmail.com>
2023-07-24 12:18:18 +0330

    did the fourth task - https://www.vuemastery.com/courses/intro-to-vue-js/list-rendering/

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ var app = new Vue({
     imageAlt: 'A pair of socks',
     linkHref: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding',
     inStock: true,
-    onSale: true
+    onSale: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      },
+    ],
+    sizes: ['S', 'M', 'L', 'XL']
   }
 })
